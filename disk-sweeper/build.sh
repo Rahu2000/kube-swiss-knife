86391bacc0a4cdef2654307d776ad05b7184c1ca
megazone3 <megazone3@finda.co.kr>
2025-08-18 15:36:28 +0900

    bugfix: add namespace options

--- disk-sweeper/build.sh
+++ disk-sweeper/build.sh
@@ -2,7 +2,9 @@
 
 REPO_IMAGE=$1
 TAG=$2
+NAMESPACE=${3:-k8s.io}
 
+echo $NAMESPACE
 # QEMU 에뮬레이터 설정
 docker run --privileged --rm tonistiigi/binfmt --install all
 
@@ -11,6 +13,6 @@ docker buildx create --name multiarch-builder --driver docker-container --use
 docker buildx inspect --bootstrap
 
 # 멀티 아키텍처 빌드 및 푸시
-docker buildx build --platform linux/amd64,linux/arm64 -t $REPO_IMAGE:$TAG --push .
+docker buildx build --platform linux/amd64,linux/arm64 -t $REPO_IMAGE:$TAG --build-arg NAMESPACE=$NAMESPACE --push .
 
 docker buildx rm multiarch-builder
--- disk-sweeper/sweeper.sh
+++ disk-sweeper/sweeper.sh
@@ -6,26 +6,38 @@ echo "Starting disk cleanup script..."
 
 NERDCTL_PATH=$(command -v nerdctl 2>/dev/null)
 CTR_PATH=$(command -v ctr 2>/dev/null)
+NAMESPACE=${1:-k8s.io}
+
 if [ -n "$NERDCTL_PATH" ] && [ -x "$NERDCTL_PATH" ]; then
   echo "nerdctl found. Running nerdctl system prune..."
-  nerdctl system prune -a -f --namespace=k8s.io 2>/dev/null
+  nerdctl system prune -a -f --namespace=$NAMESPACE 2>/dev/null
 
-  PAUSE_IMAGE=$(nerdctl images | grep pause | head -1 | awk '{print $3}')
+  LOCALHOST_PAUSE_IMAGE=$(nerdctl images --namespace=$NAMESPACE | grep localhost/kubernetes/pause | head -1 | awk '{print $3}')
 
-  if [ -z "$PAUSE_IMAGE" ]; then
-    nerdctl pull "registry.k8s.io/pause:latest"
-    PAUSE_IMAGE=$(nerdctl images | grep pause | head -1 | awk '{print $3}')
+  if [ -z "$LOCALHOST_PAUSE_IMAGE" ]; then
+    PAUSE_IMAGE=$(nerdctl images --namespace=$NAMESPACE | grep pause | head -1 | awk '{print $3}')
+    if [ -z "$PAUSE_IMAGE" ]; then
+      nerdctl pull --namespace=$NAMESPACE "registry.k8s.io/pause:latest"
+      nerdctl tag --namespace=$NAMESPACE "registry.k8s.io/pause:latest" "localhost/kubernetes/pause:latest"
+    else
+      nerdctl tag --namespace=$NAMESPACE "$PAUSE_IMAGE" "localhost/kubernetes/pause:latest"
+    fi
+    echo "Tagging pause image: localhost/kubernetes/pause"
   fi
-
-  echo "Tagging pause image: $PAUSE_IMAGE"
-  nerdctl --namespace=k8s.io tag "$PAUSE_IMAGE" localhost/kubernetes/pause:latest
 elif [ -n "$CTR_PATH" ] && [ -x "$CTR_PATH" ]; then
   echo "ctr found. Disk 공간 확보를 위해 불필요한 컨테이너와 이미지를 삭제합니다."
-  # 모든 이미지 삭제
-  ctr --namespace k8s.io images list -q | xargs -r ctr --namespace k8s.io images remove
-  # pause 이미지 다운로드 및 태깅
-  ctr images pull registry.k8s.io/pause:latest
-  ctr images tag registry.k8s.io/pause:latest localhost/kubernetes/pause:latest
+  # pause 이미지를 제외한 모든 이미지 삭제
+  for image in $(ctr --namespace $NAMESPACE images list -q); do
+    if ! echo "$image" | grep -q "pause"; then
+      ctr --namespace $NAMESPACE images remove "$image"
+    fi
+  done
+
+  # pause 이미지가 없으면 다운로드 및 태깅
+  if ! ctr --namespace $NAMESPACE images list | grep -q pause; then
+    ctr images pull registry.k8s.io/pause:latest
+    ctr images tag registry.k8s.io/pause:latest localhost/kubernetes/pause:latest
+  fi
 else
   echo 'Neither nerdctl nor ctr is installed.'
 fi
