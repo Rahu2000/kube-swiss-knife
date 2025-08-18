81cec80a8447d20ce304a007c84e01bd06730b51
megazone3 <megazone3@finda.co.kr>
2025-08-18 10:48:15 +0900

    feat: 일부 puase 이미지가 복구 되지 못하는 오류 수정

--- disk-sweeper/sweeper.sh
+++ disk-sweeper/sweeper.sh
@@ -4,26 +4,30 @@ set -x
 
 echo "Starting disk cleanup script..."
 
-if command -v nerdctl &> /dev/null; then
+NERDCTL_PATH=$(command -v nerdctl 2>/dev/null)
+CTR_PATH=$(command -v ctr 2>/dev/null)
+if [ -n "$NERDCTL_PATH" ] && [ -x "$NERDCTL_PATH" ]; then
   echo "nerdctl found. Running nerdctl system prune..."
   nerdctl system prune -a -f --namespace=k8s.io 2>/dev/null
-  PAUSE_IMAGE=$(nerdctl images --namespace=k8s.io | grep pause | head -1 | awk '{print $3}')
+
+  PAUSE_IMAGE=$(nerdctl images | grep pause | head -1 | awk '{print $3}')
+
+  if [ -z "$PAUSE_IMAGE" ]; then
+    nerdctl pull "registry.k8s.io/pause:latest"
+    PAUSE_IMAGE=$(nerdctl images | grep pause | head -1 | awk '{print $3}')
+  fi
 
   echo "Tagging pause image: $PAUSE_IMAGE"
-  nerdctl --namespace=k8s.io tag $PAUSE_IMAGE localhost/kubernetes/pause:latest
-elif command -v crictl &> /dev/null; then
-  echo "crictl found. Running crictl cleanup..."
-  RUNNING_IMAGES=$(crictl ps --quiet --output json | jq -r '.containers[].imageRef')
-  for image in $(crictl images --quiet); do
-    if ! echo "$RUNNING_IMAGES" | grep -q "$image"; then
-      if crictl inspecti "$image" 2>/dev/null | jq -r '.status.repoTags[]' | grep -qv pause; then
-        echo "Removing image: $image"
-        crictl rmi "$image"
-      fi
-    fi
-  done
+  nerdctl --namespace=k8s.io tag "$PAUSE_IMAGE" localhost/kubernetes/pause:latest
+elif [ -n "$CTR_PATH" ] && [ -x "$CTR_PATH" ]; then
+  echo "ctr found. Disk 공간 확보를 위해 불필요한 컨테이너와 이미지를 삭제합니다."
+  # 모든 이미지 삭제
+  ctr --namespace k8s.io images list -q | xargs -r ctr --namespace k8s.io images remove
+  # pause 이미지 다운로드 및 태깅
+  ctr images pull registry.k8s.io/pause:latest
+  ctr images tag registry.k8s.io/pause:latest localhost/kubernetes/pause:latest
 else
-  echo 'Neither nerdctl nor crictl is installed.'
+  echo 'Neither nerdctl nor ctr is installed.'
 fi
 
 echo "Disk cleanup script completed."
